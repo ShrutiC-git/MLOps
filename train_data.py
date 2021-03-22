b5190981e3c0b1eb96c1a520f06cb30dfcde8e1c
Shruti Chaturvedi <shrutichaturvedi16.sc@gmail.com>
2021-03-22 19:03:16 +0530

    Using LDA

--- train_data.py
+++ train_data.py
@@ -10,7 +10,7 @@ import json
 from sklearn.preprocessing import Normalizer
 import yaml
 import pickle
-from sklearn.naive_bayes import MultinomialNB
+from  sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 df = pd.read_csv('haberman_processed.csv', index_col=0)
 
@@ -21,7 +21,7 @@ X = df.to_numpy()
 X = Normalizer().fit_transform(X)
 
 #clf = LogisticRegression(solver=yaml.safe_load(open('params.yaml'))['solver'])
-clf = MultinomialNB()
+clf = LinearDiscriminantAnalysis()
 y_pred = cross_val_predict(clf, X, y, cv = yaml.safe_load(open('params.yaml'))['cv'])
 
 acc = np.mean(y_pred==y)
